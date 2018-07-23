bcb90556a6ca2735690d812c2b00799f05141f9e
fergus <rkfiuervpiqebv>
2018-07-23 14:07:13 +0300

    fix fix

--- 2_page.php
+++ 2_page.php
@@ -3,8 +3,8 @@
   <title>ART LIFE</title>
   <meta charset="utf-8">
 </head>
+<?php include 'h.e.a.d.php';?>
 <body style="background-color: black">
-  <?php include 'h.e.a.d.php';?>
   <div style="margin-left: 250px; margin-right: 250px; background-color: white;
   border-radius: 5px">
 
