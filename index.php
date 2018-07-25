fde16453213e36b7a446ef800ac5b5b7a3754f83
fergus <rkfiuervpiqebv>
2018-07-25 14:39:58 +0300

    редактирование/добавление контента

--- index.php
+++ index.php
@@ -22,12 +22,16 @@
   <?php include 'h.e.a.d.php';?>
   <div  class="transparent">
 <div style="margin-left: 50px; margin-right: 50px;">
-<p></p>
-<br><br><br><br><br><br>
-<br><br><br><br><br><br>
-<br><br><br><br><br><br>
-<br><br><br><br><br><br>
-<br><br><br>
+<h1 align="center"><b>Доброе утро / день / вечер!</b></h1>
+<h2 align="center"><b>Приветствуем вас на официальном сайте проекта</b></h2>
+<h2 align="center"><b>"Джун Джуну"</b></h2><br>
+<h3><b>У нас можно -</b></h3>
+<p>- увидеть различные ролики с объяснениями разных фишек<br>
+   - увидеть ролики по использованию разных IT программ</p><br>
+<h3 align="right"><b>Наши Участники</b></h3>
+<p align="right">- Костян<br>
+   - Мишаня<br>
+   - Саня</p>
 <p></p>
 </div>
 </div>
