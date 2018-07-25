283648f0940ae36b917e45ee846cd367ccb64380
fergus <rkfiuervpiqebv>
2018-07-25 15:23:20 +0300

    добавление страниц (о нас, фото), редактирование файла head.php

---
+++ e.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+  <title>Home</title>
+  <meta charset="utf-8">
+  <style>
+     body {
+      background: url(/art/ikt.jpg); /* Цвет фона и путь к файлу */
+      color: #fff; /* Цвет текста */
+      background-size: 100%;
+     }
+        body {
+         background: url(/art/ikt.jpg) 100%; /*no-repid;*/ /* Цвет фона и путь к файлу */
+         color: #fff; /* Цвет текста */
+         background-size: 100%;
+        }
+        .transparent {
+       background-color: rgba(0, 0, 0, 0.5); /* Цвет фона и значение прозрачности */
+       padding: 10px; /* Поля вокруг текста */
+       color: #fff; /* Цвет текста */
+       margin-left: 250px;
+       margin-right: 250px;
+       border-radius: 5px
+      }
+    </style>
+</head>
+<body>
+  <?php include 'h.e.a.d.php';?>
+  <div  class="transparent">
+<div style="margin-left: 50px; margin-right: 50px;">
+
+</div>
+</div>
+</body>
--- h.e.a.d.php
+++ h.e.a.d.php
@@ -3,13 +3,13 @@
 <nav>
 <ul class="css-menu-3">
  <li><a href="/index.php">Главная</a></li>
- <li><a href="/a.php">Отзывы о проекте</a></li>
- <li><a href="/b.php">.</a></li>
- <li><a href="/c.php">.</a></li>
- <li><a href="/d.php">.</a></li>
- <li><a href="/5_page.php">.</a></li>
- <li><a href="/1_page.php">.</a></li>
- <li><a href="/2_page.php">.</a></li>
+ <li><a href="/b.php">Джун джуну</a></li>
+ <li><a href="/c.php">Видео</a></li>
+ <li><a href="/d.php">Доп.материал</a></li>
+ <li><a href="/e.php">О нас</a></li>
+ <li><a href="/1_page.php">Live Фото</a></li>
+ <li style=""><a href="/a.php">Отзывы о проекте</a></li>
+ <!--<li><a href="/2_page.php">.</a></li>
  <li><a href="/3_page.php">.</a></li>
  <li><a href="/4_page.php">.</a></li>
  <li><a href="/5_page.php">.</a></li>
@@ -18,7 +18,7 @@
  <li><a href="/3_page.php">.</a></li>
  <li><a href="/4_page.php">.</a></li>
  <li><a href="/5_page.php">.</a></li>
- <li><a href="/5_page.php">.</a></li>
+ <li><a href="/5_page.php">.</a></li>-->
 <ul>
 </nav>
 </div>
